a81e01cda3ea6ab711ea39d4a10d9bc94fe8fd8c
ranchu1 <rchu@vols.utk.edu>
2019-07-12 06:30:03 -0400

    Made a new function to read-in 1D yahil data.

--- rclib/IO.py
+++ rclib/IO.py
@@ -1,7 +1,8 @@
 def IO_WeakLib_EOS():
    print("hello from IO.IO_WeakLib_EOS")
 
-def test_FLASH_IO( dir, filename, ID ):
+# ------------------------------------------------------------
+def test_FLASH_IO_2D( dir, filename, ID ):
   import h5py
   import numpy as np
   import matplotlib.pyplot as plt
@@ -33,3 +34,39 @@ def test_FLASH_IO( dir, filename, ID ):
   
   return( time, block_size, coordinates, bounding_box, \
   veltot, dens, dena, pres, prsa )
+
+# ------------------------------------------------------------
+def test_FLASH_IO_1D( dir, filename, ID ):
+  import h5py
+  import numpy as np
+  import matplotlib.pyplot as plt
+  import time
+
+  FileName = dir+filename+str(ID).zfill(4)
+  print( 'filename =', FileName )
+  f = h5py.File(FileName, 'r')
+  
+  real_scalars = f['real scalars']
+  time = real_scalars[0][1]
+  print( 'time =', time ,' s')
+  block_size = f['block size']
+  coordinates = f['coordinates']
+  bounding_box = f['bounding box']
+  velx = f['velx']
+  vely = f['vely']
+  velz = f['velz']
+  # Assume 2DCylindrical or Cartesian
+  veltot = np.square(velx) + np.square(vely) \
+           + np.square(velz)
+  veltot = np.sqrt( veltot )
+
+  dens = f['dens']
+  dena = f['dena']
+  prsa = f['prsa']
+  pres = f['pres']
+  
+  
+  return( time, block_size, coordinates, bounding_box, \
+  veltot, dens, dena, pres, prsa )
+
+# ------------------------------------------------------------
